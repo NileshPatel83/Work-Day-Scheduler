d0969abaa58db72fa328ea7acf5f18a454dc18dc
NileshPatel83 <hselin_83@yahoo.com>
2023-01-14 09:47:04 +1000

    getHour method updated.

--- assets/js/script.js
+++ assets/js/script.js
@@ -38,9 +38,14 @@ const pastClass = 'past';
 const presentClass = 'present';
 const futureClass = 'future';
 
-//Constants
+//Visit rows in text area.
 const visibleRows = 3;
-const standardWorkHours = ['9 AM', '10 AM', '11 AM', '12 PM', '1 PM', '2 PM', '3 PM', '4 PM', '5 PM'];
+
+//Attribute used to get index of time block.
+const dataIndex = 'data-index';
+
+//Standard working hours time array.
+const standardWorkHours = ['9AM', '10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM', '5PM'];
 
 $(function () {
 
@@ -50,14 +55,35 @@ $(function () {
   //Adds time block for standard working hours.
   addTimeBlocks();
 
+  //TODO
+  //Display activities from local storage.
+
+
 
 });
+
+// //Event handler for time block container and then get button target.
+// timeBlockContainerEl.on('click', '.saveBtn', processDayScheduler);
+
+// function processDayScheduler(event){
+//     event.preventDefault();
+
+//     let schIndex = parseInt($(this).attr(dataIndex));
+
+//     let time = timeBlockContainerEl.children().eq(schIndex).children('div').text();
+    
+//     let schedule = timeBlockContainerEl.children().eq(schIndex).children('textarea').val();
+    
+// }
+
 
 //Adds time block for standard working hours.
 function addTimeBlocks(){
 
   //Loops through each standard working hour.
-  standardWorkHours.forEach(stdHour =>{
+  for (let i = 0; i < standardWorkHours.length; i++){
+
+    let stdHour = standardWorkHours[i];
 
     //Converts standard working hour to 24 hour format.
     let hour = getHour(stdHour);
@@ -68,7 +94,7 @@ function addTimeBlocks(){
     //Creates a div element, sets the id and time block classes.
     //This element represent the row for each time block.
     let blockDivEl = $('<div>');
-    blockDivEl.attr('id', stdHour);
+    // blockDivEl.attr('id', stdHour);
     blockDivEl.addClass(className);
 
     //Creates a div element, sets the value standard time and classes.
@@ -83,11 +109,14 @@ function addTimeBlocks(){
     textAreaEl.addClass(textAreaElClasses.join(' '));
     textAreaEl.attr('rows', visibleRows);
 
-    //Creates a button element and sets classes and attribute.
+    //Creates a button element and sets classes and attributes.
+    //Data-index attribure will be used when user click save button indenitfy which row of time block is clicked.
+    //Using this index, parent row block element can be obtained from main contaier which then can be used to get time and activitt text to be saved to local storage.
     //This element represents as save button.
     let saveBtnEl = $('<button>');
     saveBtnEl.addClass(saveBtnElClasses.join(' '));
     saveBtnEl.attr('aria-label', 'save');
+    saveBtnEl.attr(dataIndex, i);
 
     //Creates an iamge element for save button element and sets classes and attributes.
     //Adds the image element to save button element.
@@ -101,7 +130,7 @@ function addTimeBlocks(){
 
     //Adds the time block row element to main time display container element.
     timeBlockContainerEl.append(blockDivEl);
-  });
+  }
 }
 
 //Gets all classes including past, present or future for time element.
@@ -132,18 +161,18 @@ function getClassName(hour){
 //Converts standard working hour to 24 hour format.
 function getHour(stdHour){
 
-  //Splits the standard working time to time number (e.g. 9) and time format (e.g. AM).
-  let array = stdHour.split(' ');
-
-  //Converts time to number.
-  let hour = parseInt(array[0], 10);
+  //Gets time value from standard working hour (e.g. Gets 9 from 9AM and 11 from 11AM).
+  let timeValue = stdHour.substr(0, stdHour.length - 2);
 
   //Gets the time format e.g. AM/ PM.
-  let format = array[1];
+  let timeFormat = stdHour.substr(stdHour.length - 2);
+
+  //Converts time to number.
+  let hour = parseInt(timeValue, 10);
 
   //If the time number is either 12 or time format is PM, then adds 12 to get 24 hour time number.
   //e.g. 9am = 9, 12pm = 12, 2pm = 14.
-  if(format === 'PM' && hour !== 12) hour += 12;
+  if(timeFormat === 'PM' && hour !== 12) hour += 12;
 
   return hour;
 }
