31388b14f67c321ead314ce9990aeb4575c50bc6
NileshPatel83 <hselin_83@yahoo.com>
2023-01-14 11:07:24 +1000

    HTML and Js files are cleanded up.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,3 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-
-// TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-
-
-
-
 //DOM elements.
 let currentDateEl = $('#currentDay');
 let timeBlockContainerEl = $('#time-block-container');
@@ -149,6 +123,7 @@ function getLocalScheduleStorage(){
 
   let schStorage = [];
 
+  //Gets the schedule storage and converts it into an array of objects.
   let storage = localStorage.getItem(schKey);   
   if(storage !== null){
       schStorage = JSON.parse(storage);
@@ -175,7 +150,6 @@ function addTimeBlocks(){
     //Creates a div element, sets the id and time block classes.
     //This element represent the row for each time block.
     let blockDivEl = $('<div>');
-    // blockDivEl.attr('id', stdHour);
     blockDivEl.addClass(className);
 
     //Creates a div element, sets the value standard time and classes.
